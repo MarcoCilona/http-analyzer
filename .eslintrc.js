c2e4c6210b391ed98eeb04255fec6392707fb204
a03u <marco.cilona@gft.com>
2020-01-22 23:53:54 +0100

    added result card component

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'lines-between-class-members': ['error', 'never'],
     'max-len': ['error', { 'code': 120 }]
   },
   parserOptions: {
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.container
+.container.ph-4
   .row.align-center.mv-4
     .grey_medium.pa-2.br-4.width-80.d-flex.search-bar
       Select
@@ -7,8 +7,19 @@
       Button
   .row.align-center
     .column
-      .text-align-center.fs-50.fw-800 200
+      .text-align-center.fs-45.fw-700 200
       div Everything is fine!
+  .row.overflow-x-auto.mt-4
+    ResultCard(title='Url info')
+      template(#content)
+        ResultCardRow(title='Domain', value='www.google.it')
+        ResultCardRow(title='Scheme', value='HTTP')
+        ResultCardRow(title='Path', value='home')
+    ResultCard.ml-5(title='Url info')
+      template(#content)
+        ResultCardRow(title='Domain', value='www.google.it')
+        ResultCardRow(title='Scheme', value='HTTP')
+        ResultCardRow(title='Path', value='home')
 </template>
 
 <script lang="ts">
@@ -17,12 +28,16 @@ import { Component, Vue } from 'vue-property-decorator';
 // Components
 import Button from './components/ui/Button.vue';
 import Input from './components/ui/Input.vue';
+import ResultCard from './components/ui/ResultCard.vue';
+import ResultCardRow from './components/ui/ResultCardRow.vue';
 import Select from './components/ui/Select.vue';
 
 @Component({
   components: {
     Button,
     Input,
+    ResultCard,
+    ResultCardRow,
     Select,
   },
 })
@@ -31,12 +46,21 @@ export default class App extends Vue {}
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.container {
+  .row:nth-child(3) {
+    order: 3;
+  }
 }
 
 @media only screen and (min-width: 425px) {
