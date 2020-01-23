7b803521f3fe86bf214601dc6a8479496658252b
a03u <marco.cilona@gft.com>
2020-01-23 01:11:16 +0100

    Components refactor

---
+++ src/components/Search.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+.grey_medium.pa-2.br-4.width-80.d-flex.search-bar
+  Select
+  Input.mh-4
+  Button(v-if='!hideSubmitButton', @send-request='$emit("submit")')
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+// Components
+import Button from './ui/Button.vue';
+import Input from './ui/Input.vue';
+import Select from './ui/Select.vue';
+
+@Component({
+  components: {
+    Button,
+    Input,
+    Select,
+  },
+})
+export default class Search extends Vue {
+  @Prop({ type: Boolean }) hideSubmitButton?: boolean;
+}
+</script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,15 +1,12 @@
 <template lang="pug">
-.container.ph-4
-  .row.align-center.mv-4
-    .grey_medium.pa-2.br-4.width-80.d-flex.search-bar
-      Select
-      Input.mh-4
-      Button(@send-request='handleSendRequest')
-  .row.align-center
+PageLayout(:share-link='shareLink')
+  template(#request-form)
+    Search(@submit='handleSubmit')
+  template(#request-status)
     .column
       .text-align-center.fs-45.fw-700 200
       div Everything is fine!
-  .row.overflow-x-auto.mt-4
+  template(#request-results)
     ResultCard(title='Url info')
       template(#content)
         ResultCardRow(title='Domain', value='www.google.it')
@@ -20,57 +17,29 @@
         ResultCardRow(title='Domain', value='www.google.it')
         ResultCardRow(title='Scheme', value='HTTP')
         ResultCardRow(title='Path', value='home')
-  .row.mt-8.align-center
-    .direction-column
-      .fs-20.uppercase.text-align-center.mb-2 Share
-      .ph-4.pv-1.grey_medium.br-20 {{ shareLink }}
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
 // Components
-import Button from '../components/ui/Button.vue';
-import Input from '../components/ui/Input.vue';
+import PageLayout from '../components/PageLayout.vue';
+import Search from '../components/Search.vue';
 import ResultCard from '../components/ui/ResultCard.vue';
 import ResultCardRow from '../components/ui/ResultCardRow.vue';
-import Select from '../components/ui/Select.vue';
 
 @Component({
   components: {
-    Button,
-    Input,
     ResultCard,
+    PageLayout,
+    Search,
     ResultCardRow,
-    Select,
   },
 })
 export default class HomePage extends Vue {
   shareLink?: string = '';
-  handleSendRequest() {
+  handleSubmit() {
     this.shareLink = `${window.location.origin}/#/1212312`;
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.container {
-  .row:nth-child(3) {
-    order: 3;
-  }
-  .row:nth-child(4) {
-    order: 4;
-  }
-}
-
-@media only screen and (min-width: 425px) {
-  .container {
-    .row:nth-child(1) {
-      order: 2;
-    }
-    .row:nth-child(2) {
-      order: 1;
-    }
-  }
-}
-</style>
